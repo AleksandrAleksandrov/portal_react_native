9ba625707d79357358cc5b5a1d0891b1b3c40c1f
AleksandrAleksandrov <aleksandrggggg@gmail.com>
2017-09-11 08:56:41 +0300

    Implemented authorization, and save token

---
+++ src/actions/AuthActions.js
@@ -0,0 +1,54 @@
+import { Actions } from 'react-native-router-flux';
+import { AsyncStorage } from 'react-native';
+import axios from 'axios';
+import {
+  URL,
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER,
+  LOGIN_USER_SECCESS,
+  LOGIN_USER_FAIL,
+  TOKEN
+} from './types';
+import * as serviceREST from '../services/serviceREST';
+
+export const emailChanged = (text) => {
+  return {
+    type: EMAIL_CHANGED,
+    payload: text
+  };
+};
+
+export const passwordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: text
+  };
+};
+
+export const loginUser = (email, password) => (dispatch) => {
+  console.log(email);
+  serviceREST.postLogin({
+    email,
+    password
+  }).then((response) => {
+  console.log(response);
+  dispatch(loginUserSuccess(response.data))
+})
+.catch((error) => {
+  console.log(error);
+});
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL });
+};
+
+const loginUserSuccess = (user) => {
+  console.log(user);
+  AsyncStorage.setItem(TOKEN, user.key);
+  return {
+    type: LOGIN_USER_SECCESS,
+    payload: user
+  };
+};
---
+++ src/components/LoginForm.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Text, AsyncStorage } from 'react-native';
+import { connect } from 'react-redux';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
+import { Card, CardSection, Input, Button, Spinner } from './common';
+
+class LoginForm extends Component {
+   componentWillMount() {
+    AsyncStorage.getItem('token').then((settingsStr) => {
+    console.warn(settingsStr);
+  }).catch((error) => {
+    console.warn(error);
+  });
+}
+
+  onEmailChanged(text) {
+    this.props.dispatch(emailChanged(text));
+  }
+
+  onPasswordChanged(text) {
+    this.props.dispatch(passwordChanged(text));
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+    console.warn('Email: ' + email);
+    this.props.dispatch(loginUser(email, password));
+  }
+
+  getToken() {
+    AsyncStorage.getItem('token').then((settingsStr) => {
+    return settingsStr;
+  });
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@mail.com"
+            onChangeText={this.onEmailChanged.bind(this)}
+            value={this.props.email}
+          />
+        </CardSection>
+      <CardSection>
+        <Input
+          secureTextEntry
+          label="Пароль"
+          placeholder="Пароль"
+          onChangeText={this.onPasswordChanged.bind(this)}
+          value={this.props.password}
+        />
+      </CardSection>
+      <CardSection>
+        <Button onPress={this.onButtonPress.bind(this)}>
+          Войти
+        </Button>
+      </CardSection>
+      <CardSection>
+        <Text>
+          {this.props.user && this.props.user.key}
+        </Text>
+      </CardSection>
+      </Card>
+    );
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
+
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error, loading, user } = auth;
+
+  return { email, password, error, loading, user };
+};
+
+export default connect(mapStateToProps)(LoginForm);
